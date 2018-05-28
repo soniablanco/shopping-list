af6c79d4f527944a62e90ba9f52bb1e4b78dabe4
Sonia Blanco <soniacarolina.blanco@gmail.com>
2018-05-28 23:53:09 +0100

    Add project

---
+++ app/src/main/java/com/piscos/soni/shoppinglist/AllItemsActivity.java
@@ -0,0 +1,13 @@
+package com.piscos.soni.shoppinglist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_items);
+    }
+}
