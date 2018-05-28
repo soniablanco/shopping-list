51238aecacc3d5b71d2e4028056cfe2a17e7b637
Sonia Blanco <soniacarolina.blanco@gmail.com>
2018-05-29 00:43:11 +0100

    Classes and layouts to display the list

---
+++ app/src/main/java/com/piscos/soni/shoppinglist/ProductListItem.java
@@ -0,0 +1,13 @@
+package com.piscos.soni.shoppinglist;
+
+public class ProductListItem {
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+}
---
+++ app/src/main/java/com/piscos/soni/shoppinglist/ProductListItemsTestData.java
@@ -0,0 +1,31 @@
+package com.piscos.soni.shoppinglist;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductListItemsTestData {
+    private static ProductListItemsTestData sProductListItemsTestData;
+    private List<ProductListItem> mProductListItems;
+
+    public static ProductListItemsTestData get(Context context){
+        if (sProductListItemsTestData == null){
+            sProductListItemsTestData = new ProductListItemsTestData(context);
+        }
+        return sProductListItemsTestData;
+    }
+
+    private ProductListItemsTestData(Context context){
+        mProductListItems = new ArrayList<>();
+        for (int i=0; i<10;i++){
+            ProductListItem item = new ProductListItem();
+            item.setName("Product " + i);
+            mProductListItems.add(item);
+        }
+    }
+
+    public List<ProductListItem> getProductListItems(){
+        return mProductListItems;
+    }
+}
