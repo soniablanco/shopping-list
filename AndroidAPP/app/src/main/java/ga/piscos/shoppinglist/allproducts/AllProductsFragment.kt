d0716745458fff57d6fbed85203e8cd2ba8eb75b
Sonia <sonia@sonia.com>
2020-10-05 11:50:17 +0100

    preparing new list

---
+++ AndroidAPP/app/src/main/java/ga/piscos/shoppinglist/allproducts/AllProductsFragment.kt
@@ -0,0 +1,71 @@
+package ga.piscos.shoppinglist.allproducts
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ga.piscos.shoppinglist.R
+import kotlinx.android.synthetic.main.allproducts_all_products_fragment.*
+
+class AllProductsFragment: Fragment() {
+
+
+    companion object {
+
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.allproducts_all_products_fragment, container, false)
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rv_products_list.layoutManager = LinearLayoutManager(activity)
+        rv_products_list.addItemDecoration(DividerItemDecoration(rv_products_list.context, DividerItemDecoration.VERTICAL))
+        rv_products_list.setHasFixedSize(true)
+    }
+
+    private inner class ProductsListItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(product: ProductItem, onclickListener: (ProductItem) -> Unit)= with(itemView){
+
+            tvProductNameTextView.text = product.name.replace("++","").replace("**","")
+            qtyText.text= if (product.filled) "✓" else "✖"
+
+
+            setOnClickListener { onclickListener(product) }
+        }
+    }
+
+
+
+    private inner class ProductsListItemAdapter(private var elements:MutableList<ProductItem> = arrayListOf(), val onclickListener: (ProductItem) -> Unit
+    ) : RecyclerView.Adapter<ProductsListItemHolder>() {
+
+
+        fun updateProducts(stockList:List<ProductItem>){
+            elements.clear()
+            elements.addAll(stockList)
+            notifyDataSetChanged()
+        }
+
+        override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ProductsListItemHolder {
+            return ProductsListItemHolder(LayoutInflater.from(activity).inflate(R.layout.action_stock_summary_list_item, viewGroup, false))
+        }
+
+        override fun onBindViewHolder(holder: ProductsListItemHolder, position: Int) = holder.bind(elements[position], onclickListener)
+
+        override fun getItemCount(): Int {  return elements.size  }
+    }
+
+
+}
---
+++ AndroidAPP/app/src/main/java/ga/piscos/shoppinglist/allproducts/ProductItem.kt
@@ -0,0 +1,3 @@
+package ga.piscos.shoppinglist.allproducts
+
+class ProductItem
