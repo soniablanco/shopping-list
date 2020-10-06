0a1c92a3483db8a2ae843ab680bb97519983020d
Sonia <sonia@sonia.com>
2020-10-06 13:44:21 +0100

    recycler view

--- AndroidAPP/app/src/main/java/ga/piscos/shoppinglist/allproducts/ProductFragment.kt
+++ AndroidAPP/app/src/main/java/ga/piscos/shoppinglist/allproducts/ProductFragment.kt
@@ -1,11 +1,16 @@
 package ga.piscos.shoppinglist.allproducts
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ga.piscos.shoppinglist.R
+import kotlinx.android.synthetic.main.allproducts_product_layout.*
 
 class ProductFragment : Fragment() {
 
@@ -20,5 +25,41 @@ class ProductFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         return inflater.inflate(R.layout.allproducts_product_layout, container, false)
+
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rv_stores_list.layoutManager = LinearLayoutManager(activity)
+        rv_stores_list.addItemDecoration(DividerItemDecoration(rv_stores_list.context, DividerItemDecoration.VERTICAL))
+        rv_stores_list.setHasFixedSize(true)
+        val adapter = ProductsListItemAdapter{
+        }
+        rv_stores_list.adapter = adapter
+    }
+
+
+    private inner class ProductsListItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(product: ProductItem, onclickListener: (ProductItem) -> Unit)= with(itemView){
+            setOnClickListener { onclickListener(product) }
+        }
+    }
+    private inner class ProductsListItemAdapter(private var elements:MutableList<ProductItem> = arrayListOf(), val onclickListener: (ProductItem) -> Unit
+    ) : RecyclerView.Adapter<ProductFragment.ProductsListItemHolder>() {
+
+
+        fun updateProducts(stockList:List<ProductItem>){
+            elements.clear()
+            elements.addAll(stockList)
+            notifyDataSetChanged()
+        }
+
+        override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ProductFragment.ProductsListItemHolder {
+            return ProductsListItemHolder(LayoutInflater.from(activity).inflate(R.layout.allproducts_product_item, viewGroup, false))
+        }
+
+        override fun onBindViewHolder(holder: ProductFragment.ProductsListItemHolder, position: Int) = holder.bind(elements[position], onclickListener)
+
+        override fun getItemCount(): Int {  return elements.size  }
+    }
+
 }
