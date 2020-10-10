7e514d1d75d745711a3b8916864858bb714a509a
Sonia <sonia@sonia.com>
2020-10-10 23:27:08 +0100

    planning stage

--- AndroidAPP/app/src/main/java/ga/piscos/shoppinglist/MainActivity.kt
+++ AndroidAPP/app/src/main/java/ga/piscos/shoppinglist/MainActivity.kt
@@ -1,19 +1,31 @@
 package ga.piscos.shoppinglist
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ga.piscos.shoppinglist.allproducts.AllProductsFragment
 import ga.piscos.shoppinglist.planning.ProductListFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         loadFragment(ProductListFragment.newInstance())
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
     }
 
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.plan -> loadFragment(ProductListFragment.newInstance())
+            R.id.allproducts -> loadFragment(AllProductsFragment.newInstance())
+        }
+        return true
+    }
 
     private fun loadFragment(fragment: Fragment?): Boolean {
         //switching fragment
